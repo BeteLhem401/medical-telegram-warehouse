9894cc0dea7a98322a4e6cfcc395d0881739e07d
BeteLhem401 <hailubetelhem3@gmail.com>
2026-07-01 20:48:15 +0300

    feat(task2): add dim_channels and dim_dates, wire fct_messages foreign keys

--- medical_warehouse/models/marts/fct_messages.sql
+++ medical_warehouse/models/marts/fct_messages.sql
@@ -1,16 +1,24 @@
 with messages as (
     select * from {{ ref('stg_telegram_messages') }}
+),
+
+channels as (
+    select * from {{ ref('dim_channels') }}
+),
+
+dates as (
+    select * from {{ ref('dim_dates') }}
 )
 
 select
-    message_id,
-    channel_name,
-    message_date,
-    message_time,
-    message_text,
-    has_media,
-    image_path,
-    views,
-    forwards,
-    scraped_at
-from messages
+    m.message_id,
+    c.channel_key,
+    d.date_key,
+    m.message_text,
+    m.message_length,
+    m.view_count,
+    m.forward_count,
+    m.has_image
+from messages m
+left join channels c on m.channel_name = c.channel_name
+left join dates d on m.message_date = d.full_date
